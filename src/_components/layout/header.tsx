5872bef2771cf5eab6046d98f4305dc62a2963cb
choitjddn0311 <cswcjw311427@naver.com>
2025-12-16 11:14:45 +0900

    chore: change url

--- src/_components/layout/header.tsx
+++ src/_components/layout/header.tsx
@@ -3,7 +3,7 @@ import ThemeToggle from "../common/themeToggle";
 
 const Header = () => {
     return (
-        <header className="w-full h-25 flex justify-center bg-[#fafafa] text-foreground sticky top-0 z-10">
+        <header className="w-full h-25 flex justify-center text-foreground sticky top-0 z-10">
             <div className="w-300 flex justify-between items-center ">
                 <Link href="/" className="w-50 h-full content-center font-bold text-4xl text-cyan-400">
                 {/* className="text-s hidden group-hover:inline" sub className */}
@@ -15,7 +15,7 @@ const Header = () => {
                             <li className="font-semibold"><Link href="/">home</Link></li>
                             <li className="font-semibold"><Link href="posts">posts</Link></li>
                             <li className="font-semibold"><Link href="/about">about me</Link></li>
-                            <li className="font-semibold"><Link href="/contacts">contact</Link></li>
+                            <li className="font-semibold"><Link href="/contact">contact</Link></li>
                         </ul>
                     </nav>
                     <ThemeToggle/>
